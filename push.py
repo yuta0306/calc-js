dcbdcafb20feb2b9a6338e3ec06c602b40e068dc
Yubo <you-2001-3-6@ezweb.ne.jp>
2020-11-08 17:52:42 +0900

    "add"

--- push.py
+++ push.py
@@ -8,6 +8,10 @@ def git_flow():
     res = subprocess.run(
         ['git', 'commit', '-m', '"add"']
     )
+    print(res)
+    res = subprocess.run(
+        ['git', 'push', ' ']
+    )
 
 if __name__ == "__main__":
     git_flow()
